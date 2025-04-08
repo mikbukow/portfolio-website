3df6eef195240928837f975bd8c43457d05bf9fe
Mikaela Bukow <mikaelabukow@Mikaelas-MacBook-Pro.local>
2025-04-07 17:12:51 -0700

    made experience page responsive ,added scroll feature to navbar

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,11 @@
+import React from "react"
+
 export default function About() {
-    return <h1>About</h1>
+    return (
+        <div className="about-me">
+            <div className="about-me-container">
+                <p>Hi! I'm Mikaela, a software engineer with a degree in Computer Science currently based in San Francisco. </p>
+            </div>
+        </div>
+    );
 }
