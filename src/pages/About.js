b7b9fb03898ff68b5283e8e80372973d7c96a2e5
Mikaela Bukow <mikaelabukow@Mikaelas-MacBook-Pro.local>
2025-04-08 22:42:30 -0700

    added beginning of about me page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,23 @@
 import React from "react";
 import './About.css';
+import { Button } from '../components/Button';
+import me from '../assets/me.jpeg';
+
 
 export default function About() {
     return (
         <div className='about-me'>
             <div className="about-me-container">
-                <h1>About - Coming Soon</h1>
+                <span className="title">
+                    <h2>Hi! I'm Mikaela Bukow. I’m a full time software engineer located in San Francisco, CA. </h2>
+                    <h3> I find joy in creating things, whether it be paintings, automated solutions or some new ceramic mugs</h3>
+                    <Button className='more-about-button' path={'#more-about'}>
+                        Learn More About Me - Coming Soon
+                    </Button>
+                </span>
+                <span className="pics">
+                    <img src={me} />
+                </span>
             </div>
         </div>
     );
